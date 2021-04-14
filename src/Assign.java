f3143ccf79e3bbe4de2b2e865130e7697416391a
hongeinh <honganh3179@gmail.com>
2021-04-14 23:32:07 +0700

    assigment and distance calculating formula

---
+++ src/Assign.java
@@ -0,0 +1,45 @@
+public class Assign {
+
+
+	public static void main(String[] args) {
+		int [][] arr = {
+			{ 0,	0,	1,	1,	0,	0,	0,	0,	0,	0,	0 },
+			{ 1,	0,	0,	0,	0,	1,	1,	0,	0,	0,	0 },
+			{ 1,	1,	0,	0,	0,	0,	0,	1,	1,	1,	0 },
+			{ 0,	1,	1,	1,	0,	0,	0,	0,	0,	1,	0 },
+			{ 0,	0,	0,	1,	0,	1,	1,	0,	1,	0,	1 },
+			{ 0,	0,	0,	0,	0,	0,	1,	0,	0,	0,	1 },
+			{ 0,	0,	0,	1,	0,	0,	0,	0,	1,	0,	0 },
+			{ 0,	0,	0,	0,	0,	0,	0,	1,	0,	0,	1 },
+			{ 1,	1,	0,	0,	0,	0,	1,	0,	0,	0,	1 },
+			{ 0,	0,	0,	0,	0,	0,	0,	0,	0,	0,	1 },
+			{ 1,	1,	0,	0,	1,	1,	1,	1,	1,	0,	1 },
+			{ 1,	0,	1,	0,	0,	0,	0,	0,	1,	0,	1 }
+		};
+		double assign = 0.0;
+		double count;
+		double total;
+	  	for (int i = 0; i < 11; i++) {
+	  		count = 0; total = 0;
+			for (int j = 0; j < 12; j++) {
+	  	    	if(arr[j][i] == 1) {
+	  	    		total = total + 1;
+					if (j < 5) {
+						count = count + 1;
+					}
+				}
+			}
+			if(count == 1) {
+				count = 0;
+			}
+			if (arr[8][i] == 1 && arr[9][i] == 1) {
+				count = count + 2;
+			}
+
+			if(total > 0) {
+				assign = assign + (1 - count/total);
+			}
+		}
+		System.out.println("Assign = " + assign/11);
+	}
+}
--- src/old/Task.java
+++ src/old/Task.java
@@ -1,4 +1,7 @@
-package old;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Task {
